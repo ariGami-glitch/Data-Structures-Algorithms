dd2874224be2513fd4e062debc0e5f56a0225fd4
Ariana Tran <ariana192000@hotmail.com>
2021-08-24 11:02:33 -0400

    added the family class

---
+++ Lab1/src/Family.java
@@ -0,0 +1,7 @@
+public class Family {
+    private int girls = 0;
+    private int boys = 0;
+    public Family() {
+
+    }
+}
--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Ariana Tran
+ * CPSC 340
+ *
+ */
 public class Main {
     public static void main(String[] args) {
 
