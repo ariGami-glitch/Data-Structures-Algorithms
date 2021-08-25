6c805b7825f395e2ec87edc1a716ae03b2c75cde
Ariana Tran <ariana192000@hotmail.com>
2021-08-25 10:54:57 -0400

    finalized the comments and added the display method

--- Lab1/src/Family.java
+++ Lab1/src/Family.java
@@ -1,4 +1,8 @@
 import java.util.Random;
+
+/**
+ * This class represents one family in a country.
+ */
 public class Family {
     //private variables for girls and boys
     private int girls;
@@ -10,38 +14,51 @@ public class Family {
         boys = 0;
     }
 
-    /* Getter method to get the number of girls the family has */
+    /**
+     * This method returns the number of girls the family have
+     *
+     * @return an int that represents the number of girls.
+     */
     public int getGirls() {
         return girls;
     }
 
-    /* Getter method to get the number of boys the family has */
+    /**
+     * This method returns the number of boys the family have
+     *
+     * @return an int that represents the number of boys
+     */
     public int getBoys() {
         return boys;
     }
 
+    /**
+     * This method represents one life cycle the family has. As in if the family
+     * has a girl, they stop, if not, they keep trying for a girl.
+     */
     public void step() {
         //instance of Random class
         Random rand = new Random();
         //variable for the random number
         int baby = rand.nextInt(2);
 
+        //this represents if the family will keep trying to have kids
         boolean done = false;
 
         while(!done) {
             //if it is 0, it is a girl
             if(baby == 0) {
                 girls += 1;
+                //the family is done trying for a girl so loop terminates
                 done = true;
             }
 
             //else, as in it is a 1, then it is a boy
             else {
                 boys += 1;
-                //System.out.print("Try again");
+                //the family tries again for a girl
                 baby = rand.nextInt(2);
             }
         }
     }
-
 }
--- Lab1/src/Lab1Main.java
+++ Lab1/src/Lab1Main.java
@@ -5,24 +5,41 @@
  *
  * I am not very good at statistics, as I do not remember anything
  * from it, I am going to guess the proportion of girls to boys in a
- * country would be 2:1 assuming if they have a boy they would try again
- * for a girl.
- *
+ * country would be 1:1 assuming if they have a boy they would try again
+ * for a girl. Based on biology, every family has a 50% chance of having a girl
+ * and 50% of having a boy.
  */
 public class Lab1Main {
+    private static int girls = 0;
+    private static int boys = 0;
+    private static double total;
+
+    /**
+     * This method represents the main
+     */
     public static void main(String[] args) {
-        Family[] families = new Family[5];
+        Family[] families = new Family[1000000];
 
         for(int i = 0; i < families.length; i++) {
             Family f = new Family();
             f.step();
-            System.out.println("Num of girls: " + f.getGirls());
-            System.out.println("Num of boys: " + f.getBoys());
-            System.out.println("----------------------------------");
+            girls += f.getGirls();
+            boys += f.getBoys();
             families[i] = f;
-
+            total = boys + girls;
         }
+        displayAnswer();
+    }
 
-
+    /**
+     * This method displays the answer to the problem. It takes the
+     * number of girls and boys and prints out the percentage of girls
+     * and boys in a place with 1000000 families, followed by the number
+     * of girls.
+     */
+    public static void displayAnswer() {
+        double percentGirls = ((double)girls / total) * 100;
+        double percentBoys = ((double)boys / total) * 100;
+        System.out.println("% girls: " + percentGirls + ", %boys: " + percentBoys + ", total number of girls: " + girls);
     }
 }
